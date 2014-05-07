63ffd1375874bbb7adb96778ce3bec83952a0217
Muneer Alhwi <alhawi.muneer@gmail.com>
2014-05-07 12:34:28 +0300

    Update timezone.php

--- timezone.php
+++ timezone.php
@@ -6,8 +6,7 @@
 <body>
 	<?php 
 		$date = date("Y/m/d h:i:s A");
-		echo "<h1>Welcome to my website.</h1> $date";
-		echo " - ". date_default_timezone_get();
+		echo "$date - ". date_default_timezone_get();
 		date_default_timezone_set("UTC");
 		$date1 = date("Y/m/d h:i:s A");
 		echo "<br /><br /> $date1 - ". date_default_timezone_get();
