57912c281247adda66d9c958d70bf8a12aa1cb4e
Muneer Alhwi <alhawi.muneer@gmail.com>
2014-05-07 11:06:38 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,11 +7,11 @@
 	<?php 
 		$date = date("Y/m/d h:i:s A");
 		echo "<h1>Welcome to my website.</h1> $date";
-		echo ": ". date_default_timezone_get();
+		echo " - ". date_default_timezone_get();
 		date_default_timezone_set("UTC");
 		$date1 = date("Y/m/d h:i:s A");
 		echo "<br /><br />". date_default_timezone_get();
-		echo ": ". $date1;
+		echo " - ". $date1;
 	?>
 </body>
 </html>
